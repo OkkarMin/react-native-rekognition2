f66ce92db44dc32b83b154e018be2e42cdea6945
Okkar Min <phoehtaungwin@gmail.com>
2019-10-17 10:44:36 +0800

    Changed CameraPage.js

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,75 +1,98 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions
-} from 'react-native'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
+import { Camera, Permissions } from 'expo-camera'
 import * as Permissions from 'expo-permissions'
-import { Camera } from 'expo-camera'
+
+import CameraToolbar from '../components/CameraToolbar'
 
 const { width: winWidth, height: winHeight } = Dimensions.get('window')
 
-export default class CameraScreen extends React.Component {
-  static navigationOptions = {
-    header: null
-  }
+export default class CameraPage extends React.Component {
+  camera = null
 
   state = {
+    captures: [],
+    capturing: null,
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back
+    cameraType: Camera.Constants.Type.back,
+    flashMode: Camera.Constants.FlashMode.off
+  }
+
+  setFlashMode = flashMode => this.setState({ flashMode })
+  setCameraType = cameraType => this.setState({ cameraType })
+  handleCaptureIn = () => this.setState({ capturing: true })
+
+  handleCaptureOut = () => {
+    if (this.state.capturing) this.camera.stopRecording()
+  }
+
+  handleShortCapture = async () => {
+    const photoData = await this.camera.takePictureAsync()
+    this.setState({
+      capturing: false,
+      captures: [photoData, ...this.state.captures]
+    })
+  }
+
+  handleLongCapture = async () => {
+    const videoData = await this.camera.recordAsync()
+    this.setState({
+      capturing: false,
+      captures: [videoData, ...this.state.captures]
+    })
   }
 
   async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA)
-    this.setState({ hasCameraPermission: status === 'granted' })
+    const camera = await Permissions.askAsync(Permissions.CAMERA)
+    const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING)
+    const hasCameraPermission =
+      camera.status === 'granted' && audio.status === 'granted'
+
+    this.setState({ hasCameraPermission })
   }
 
   render() {
-    const { hasCameraPermission } = this.state
+    const {
+      hasCameraPermission,
+      flashMode,
+      cameraType,
+      capturing,
+      captures
+    } = this.state
+
     if (hasCameraPermission === null) {
       return <View />
     } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>
-    } else {
-      return (
-        <View style={styles.preview}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row'
-              }}
-            >
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center'
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                  })
-                }}
-              >
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
-                >
-                  {' '}
-                  Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      )
+      return <Text>Access to camera has been denied.</Text>
     }
+
+    return (
+      <React.Fragment>
+        <View>
+          <Camera
+            type={cameraType}
+            flashMode={flashMode}
+            style={styles.preview}
+            ref={camera => (this.camera = camera)}
+          />
+        </View>
+
+        {/* For Gallery Preview */}
+        {/* {captures.length > 0 && <Gallery captures={captures} />} */}
+
+        <Toolbar
+          capturing={capturing}
+          flashMode={flashMode}
+          cameraType={cameraType}
+          setFlashMode={this.setFlashMode}
+          setCameraType={this.setCameraType}
+          onCaptureIn={this.handleCaptureIn}
+          onCaptureOut={this.handleCaptureOut}
+          onLongCapture={this.handleLongCapture}
+          onShortCapture={this.handleShortCapture}
+        />
+      </React.Fragment>
+    )
   }
 }
 
