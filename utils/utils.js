fe4117c336eed5b6db00b4ee9eb79655ff96cf3a
Okkar Min <phoehtaungwin@gmail.com>
2019-10-18 12:31:51 +0800

    Added postData() sendSMS() detectFacesFromAWSCollection() in utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -12,3 +12,57 @@ export const fetchData = async (url, endpoint) => {
     console.log(error)
   }
 }
+
+export const postData = async (url, endpoint, payload) => {
+  try {
+    let response = await fetch(`${url}${endpoint}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+
+    return await response.text()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const detectFacesFromAWSCollection = async (collectionName, image) => {
+  let payload = {
+    collectionName,
+    image
+  }
+
+  let result = await postData(
+    'http://ec2-3-15-165-103.us-east-2.compute.amazonaws.com/api',
+    '/detectFaces',
+    payload
+  )
+
+  return result
+}
+
+export const sendSMS = async (message, phoneNumber) => {
+  let payload = {
+    message,
+    phoneNumber
+  }
+
+  try {
+    let response = await fetch(`${url}/sendSMS`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+
+    console.log(await response.text())
+  } catch (error) {
+    console.log(error)
+  }
+}
