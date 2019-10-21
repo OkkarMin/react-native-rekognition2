5ef77e77db95371a66f26d711401a14c86ea8254
Okkar Min <phoehtaungwin@gmail.com>
2019-10-21 10:26:00 +0800

    Changed color in ToggleButton

--- components/ToggleButton.js
+++ components/ToggleButton.js
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import SwitchToggle from "react-native-switch-toggle";
-import { putData } from "../utils/utils";
+import React, { useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import SwitchToggle from 'react-native-switch-toggle'
+import { putData } from '../utils/utils'
 
-import Colors from "../constants/Colors";
+import Colors from '../constants/Colors'
 
 export default ToggleButton = ({ value, data, courseCode }) => {
-  const [switchOn, setSwitchOn] = useState(value);
+  const [switchOn, setSwitchOn] = useState(value)
 
   updateAttendance = async data => {
-    console.log(data);
-    let endPointURL = `/updateStudentAttendance`;
+    console.log(data)
+    let endPointURL = `/updateStudentAttendance`
     let payload = {
       courseCode: courseCode,
       groupID: data.groupID,
       classType: data.classType,
       date: data.date,
-      status: switchOn ? "Present" : "Absent",
-      acadYear: "2019",
-      semester: "1",
+      status: switchOn ? 'Present' : 'Absent',
+      acadYear: '2019',
+      semester: '1',
       matricNo: data.matricNo,
-      remarks: "nil"
-    };
+      remarks: 'nil'
+    }
 
     putData(
-      "http://ec2-3-15-165-103.us-east-2.compute.amazonaws.com/api",
+      'http://ec2-3-15-165-103.us-east-2.compute.amazonaws.com/api',
       endPointURL,
       payload
-    ).then(result => console.log(result));
-  };
+    ).then(result => console.log(result))
+  }
 
   return (
     <View style={styles.container}>
@@ -36,16 +36,16 @@ export default ToggleButton = ({ value, data, courseCode }) => {
 
       <SwitchToggle
         switchOn={switchOn}
-        circleColorOff="red"
-        circleColorOn="green"
+        circleColorOff={Colors.background2}
+        circleColorOn={Colors.primary}
         onPress={async () => {
-          setSwitchOn(!switchOn);
-          updateAttendance(data);
+          setSwitchOn(!switchOn)
+          updateAttendance(data)
         }}
       />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -53,12 +53,12 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 15,
     backgroundColor: Colors.background,
-    flexDirection: "row",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   greetings: {
     fontSize: 15,
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.text
   }
-});
+})
